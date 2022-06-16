3e6209e0c6b5d4ea26ddef04e10510b618900b4e
Inejka <dante12654@gmail.com>
2022-06-16 20:47:11 +0300

    Change ArrayList<Intger> to byte[]

--- src/main/java/classicChess/Rules.java
+++ src/main/java/classicChess/Rules.java
@@ -1,69 +1,86 @@
 package classicChess;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Rules {
 
-    public static List<Integer> generateMovesForTile(Board board, int y, int x) {
-        List<Integer> toReturn = new LinkedList<>();
+    public static byte[] generateMovesForTile(Board board, int y, int x) {
+        byte[] toReturn = null;
         switch (board.board[y][x]) {
-            case 'P':
-                blackPawnCheck(board, y, x, toReturn);
-                break;
             case 'p':
+                toReturn = new byte[10];
                 whitePawnCheck(board, y, x, toReturn);
+                break;
+            case 'P':
+                toReturn = new byte[10];
+                blackPawnCheck(board, y, x, toReturn);
                 break;
         }
         return toReturn;
     }
 
-    private static void whitePawnCheck(Board board, int y, int x, List<Integer> toReturn) {
+    private static void whitePawnCheck(Board board, int y, int x, byte[] toReturn) {
+        byte i = 1;
         if (emptyFieldCheck(board, y - 1, x)) {
-            toReturn.add(y - 1);
-            toReturn.add(x);
+            toReturn[1] = ((byte) (y - 1));
+            toReturn[2] = ((byte) x);
+            i += 2;
             if (y == 7 && emptyFieldCheck(board, 5, x)) {
-                toReturn.add(5);
-                toReturn.add(x);
+                toReturn[3] = ((byte) 5);
+                toReturn[4] = ((byte) x);
+                i += 2;
             }
         }
         if (x - 1 > -1 && blackFigureCheck(board, y - 1, x - 1)) {
-            toReturn.add(y - 1);
-            toReturn.add(x - 1);
+            toReturn[i] = ((byte) (y - 1));
+            toReturn[i + 1] = ((byte) (x - 1));
+            i += 2;
         }
         if (x + 1 < 8 && blackFigureCheck(board, y - 1, x + 1)) {
-            toReturn.add(y - 1);
-            toReturn.add(x + 1);
+            toReturn[i] = ((byte) (y - 1));
+            toReturn[i + 1] = ((byte) (x + 1));
+            i += 2;
         }
         if (board.board[board.y2][board.x1] == 'P' && board.x1 == board.x2 && board.y2 - board.y1 == 2 && board.y2 == y &&
                 (board.x2 - x == -1 || board.x2 - x == 1)) {
-            toReturn.add(board.y2 - 1);
-            toReturn.add((int) board.x2);
+            toReturn[i] = ((byte) (board.y2 - 1));
+            toReturn[i + 1] = (board.x2);
+            i += 2;
         }
+        toReturn[0] = i;
     }
 
-    private static void blackPawnCheck(Board board, int y, int x, List<Integer> toReturn) {
+    private static void blackPawnCheck(Board board, int y, int x, byte[] toReturn) {
+        byte i = 1;
         if (emptyFieldCheck(board, y + 1, x)) {
-            toReturn.add(y + 1);
-            toReturn.add(x);
+            toReturn[1] = ((byte) (y + 1));
+            toReturn[2] = ((byte) x);
+            i += 2;
             if (y == 1 && emptyFieldCheck(board, 3, x)) {
-                toReturn.add(3);
-                toReturn.add(x);
+                toReturn[3] = ((byte) 3);
+                toReturn[4] = ((byte) x);
+                i += 2;
             }
         }
         if (x - 1 > -1 && whiteFigureCheck(board, y + 1, x - 1)) {
-            toReturn.add(y + 1);
-            toReturn.add(x - 1);
+            toReturn[i] = ((byte) (y + 1));
+            toReturn[i + 1] = ((byte) (x - 1));
+            i += 2;
         }
         if (x + 1 < 8 && whiteFigureCheck(board, y + 1, x + 1)) {
-            toReturn.add(y + 1);
-            toReturn.add(x + 1);
+            toReturn[i] = ((byte) (y + 1));
+            toReturn[i + 1] = ((byte) (x + 1));
+            i += 2;
         }
         if (board.board[board.y2][board.x1] == 'p' && board.x1 == board.x2 && board.y2 - board.y1 == -2 && board.y2 == y &&
                 (board.x2 - x == -1 || board.x2 - x == 1)) {
-            toReturn.add(board.y2 + 1);
-            toReturn.add((int) board.x2);
+            toReturn[i] = ((byte) (board.y2 + 1));
+            toReturn[i + 1] = (board.x2);
+            i += 2;
         }
+        toReturn[0] = i;
     }
 
     private static boolean blackFigureCheck(Board board, int y, int x) {
